e1916638decd19b350db0876e9f78f98ac41546f
unknown <lyonya_ivanovskij@mail.ru>
2025-03-25 15:42:39 +0300

    added: addCard, deleteCard

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Проектная работа Mesto
 
+https://github.com/0oLeono0/mesto-project-ff
+https://0oleono0.github.io/mesto-project-ff/
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,21 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function addCard(card, deleteCallBack) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  cardElement.querySelector('.card__image').src = card.link;
+  cardElement.querySelector('.card__title').textContent = card.name;
+  cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCallBack);
+  return cardElement;
+}
 
 // @todo: Функция удаления карточки
-
+function deleteCard() {
+  this.closest('.card').remove()
+}
 // @todo: Вывести карточки на страницу
+initialCards.forEach((card) => placesList.append(addCard(card, deleteCard)));
