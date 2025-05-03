fe780182aff2b26b7c327065819423d0d1de3466
unknown <lyonya_ivanovskij@mail.ru>
2025-05-03 17:37:11 +0300

    add: likes for cards

--- src/scripts/card.js
+++ src/scripts/card.js
@@ -1,17 +1,32 @@
+// Удаление карточки
 export const removeCard = (cardEl) => {
   cardEl.remove();
 };
 
+// Переключение лайка и обновление счётчика
 export const toggleLike = (buttonEl) => {
+  const countEl = buttonEl.parentElement.querySelector(".card__like-count");
   buttonEl.classList.toggle("card__like-button_is-active");
+
+  let count = Number(countEl.textContent);
+  if (buttonEl.classList.contains("card__like-button_is-active")) {
+    count++;
+  } else {
+    count--;
+  }
+  countEl.textContent = count;
 };
 
-const createCardElement = ({ name, link }, template) => {
+const createCardElement = ({ name, link, likes = [] }, template) => {
   const cardEl = template.querySelector(".card").cloneNode(true);
   const imgEl = cardEl.querySelector(".card__image");
-  cardEl.querySelector(".card__title").textContent = name;
+  const titleEl = cardEl.querySelector(".card__title");
+  const likeCountEl = cardEl.querySelector(".card__like-count");
+
+  titleEl.textContent = name;
   imgEl.src = link;
   imgEl.alt = name;
+  likeCountEl.textContent = likes.length;
   return { cardEl, imgEl };
 };
 
